da84618b57410cc1c6208b13d4bbddf6a1c63a66
frashpikass <frashpikass@gmail.com>
2017-07-02 03:51:20 +0200

    Thread progress status update The controller is now the only class entitled to call an optimization process. Optimization progress is now being tracked through SwingWorker thread communication means. Also, batch processing of multiple instances is becoming reality.

--- src/solverController/Controller.java
+++ src/solverController/Controller.java
@@ -5,20 +5,29 @@
  */
 package solverController;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 import solverModel.ALNS;
-import solverModel.ALNSPropertiesBean;
 import solverModel.Orienteering;
-import solverModel.OrienteeringPropertiesBean;
 
 /**
  * A Controller class to solve batches of orienteering problem instances using
  * specific parameters and solvers.
  * @author Frash
  */
-public class Controller {
+public class Controller
+        extends SwingWorker<Void, OptimizationStatusMessage>
+        implements PropertyChangeListener
+{
     // General Purpose Variables
     
     /**
@@ -32,6 +41,58 @@ public class Controller {
     private List<String> modelPaths;
     
     /**
+     * Reference to the current ALNS task being solved
+     */
+    private ALNS currentALNS;
+
+    @Override
+    protected Void doInBackground() throws Exception {
+        // Initialize PropertyChangeSupport
+        getPropertyChangeSupport().addPropertyChangeListener( this );
+        
+        // Initialize the last instance number
+        this.lastInstanceNumber = 0;
+        
+        // Execute the batch of instances, one at a time
+        for(String path : this.modelPaths){
+            // Save the current model path
+            this.lastModelPath = path;
+            
+            // Initialize progress to 0
+            this.setProgress(0);
+            // Publish information about the initial status
+            this.publish(new OptimizationStatusMessage(lastModelPath, 0, 0, lastInstanceNumber, modelPaths.size()));
+            
+            // Proceed with the optimization
+            this.optimize(path);
+            
+            currentALNS.execute();
+            
+            try{
+                currentALNS.get();
+            }
+            catch(InterruptedException e){
+                System.out.println("ALNS was interrupted: "+e.getMessage());
+            }
+            catch(ExecutionException e){
+                System.out.println("What happened to ALNS? "+e.getMessage());
+            }
+            
+            // Update the batch number
+            this.lastInstanceNumber++;
+        }
+        return null;
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if("messageFromALNS".equals(evt.getPropertyName())){
+                    Controller c = (Controller) evt.getSource();
+                    c.messageReceived();
+        }
+    }
+  
+    /**
      * List of possible choices for solvers.
      */
     public enum Solvers {SOLVE_RELAXED, SOLVE_MIPS, SOLVE_ALNS};
@@ -42,23 +103,72 @@ public class Controller {
     private Solvers solver = Solvers.SOLVE_ALNS;
     
     /**
+     * Path to the last model set to be solved in the batch.
+     */
+    private String lastModelPath = "";
+    
+    /**
+     * Index of the current model being solved in the batch
+     */
+    private int lastInstanceNumber = 0;
+    
+    /**
+     * Reference to the last orienteering object created. It must be cleaned when the ALNS
+     * solver has finished processing the last model path.
+     */
+    private Orienteering lastOrienteering = null;
+    
+    /**
+     * A message from an ALNS thread
+     */
+    private OptimizationStatusMessage messageFromALNS;
+    
+    public OptimizationStatusMessage getMessageFromALNS() {
+        return messageFromALNS;
+    }
+
+    public void setMessageFromALNS(OptimizationStatusMessage messageFromALNS) {
+        OptimizationStatusMessage old = this.messageFromALNS;
+        this.messageFromALNS = messageFromALNS;
+        this.firePropertyChange("messageFromALNS", old, messageFromALNS);
+    }
+    
+    /**
      * Constructor for the controller class.
      * @param modelPaths list of model paths to solve in a batch
      * @param opb Javabean that holds all general parameters for an Orienteering solver
-     * @param apb Javabean that holds all ALNS parameters
-     * @throws Exception 
+     * @param apb Javabean that holds all ALNS parameters 
+     * @param solver the solver to use with this controller
      */
     public Controller(
             List<String> modelPaths,
             OrienteeringPropertiesBean opb,
-            ALNSPropertiesBean apb
-    ) throws Exception {
+            ALNSPropertiesBean apb,
+            Solvers solver
+    ){
         // Setup all parameters
         pb = new ParametersBean(opb, apb);
-        
         this.modelPaths = modelPaths;        
+        this.solver = solver;
+        
     }
     
+    /**
+     * Constructor for the controller class.
+     * @param modelPaths list of model paths to solve in a batch
+     * @param pb Javabean that holds all solver parameters
+     * @param solver the solver to use with this controller
+     */
+    public Controller(
+            List<String> modelPaths,
+            ParametersBean pb,
+            Solvers solver
+    ){
+        this.pb = pb;
+        this.modelPaths = modelPaths;
+        this.solver = solver;
+    }
+
     public static void main(String[] args) throws Exception {
         // TEST
         List<String> modelPaths = new ArrayList<>();
@@ -67,10 +177,32 @@ public class Controller {
         Controller c = new Controller(
                 modelPaths,
                 new OrienteeringPropertiesBean(),
-                new ALNSPropertiesBean()
+                new ALNSPropertiesBean(),
+                Solvers.SOLVE_ALNS
         );
         
-        c.optimize();
+        /*
+        c.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                if("messageFromALNS".equals(evt.getPropertyName())){
+                    Controller c = (Controller) evt.getSource();
+                    c.messageReceived();
+                }
+            }
+        });
+        */
+        //SwingUtilities.invokeLater(c);
+        c.execute();
+        try{
+            c.get();
+        }
+        catch(InterruptedException e){
+            System.out.println("Optimization interrupted. "+e.getMessage());
+        }
+        catch(ExecutionException e){
+            System.out.println("Hmm... "+e.getMessage());
+        }
     }
     
     /**
@@ -117,7 +249,9 @@ public class Controller {
         // Save the chosen parameters to the output folder
         pb.serializeToJSON();
         
+        this.lastInstanceNumber = 0;
         for(String modelPath : modelPaths){
+            this.lastModelPath = modelPath;
             // Initialize a new Orienteering object starting from the current modelPath
             Orienteering o = new Orienteering(
                     modelPath,
@@ -128,26 +262,78 @@ public class Controller {
             // and Orienteering object
             ALNS a = new ALNS(
                     o,
-                    pb.getALNSproperties()
+                    pb.getALNSproperties(),
+                    this
             );
             
             // Pick a solver and apply it
             switch(solver){
                 case SOLVE_RELAXED:
-                    o.optimizeRelaxed();
+                    a.optimizeRelaxed();
                     break;
                 
                 case SOLVE_MIPS:
-                    o.optimizeMIPS();
+                    a.optimizeMIPS();
                     break;
                 
                 case SOLVE_ALNS:
-                    a.optimize();
+                    a.optimizeALNS();
                     break;
             }
             
             // Free memory occupied by Gurobi models
             o.cleanup();
         }
+    }
+    
+    
+    
+    /**
+     * Optimizes a single instance using the solver specified through the
+     * <code>setSolver</code> method.
+     */
+    public void optimize(String modelPath) throws Exception{
+        // Save the chosen parameters to the output folder
+        pb.serializeToJSON();
+        
+        // Initialize a new Orienteering object starting from the current modelPath
+        lastOrienteering = new Orienteering(
+                modelPath,
+                pb.getOrienteeringProperties()
+        );
+        
+        currentALNS = new ALNS(
+                    lastOrienteering,
+                    pb.getALNSproperties(),
+                    this
+        );
+
+        // Initialize a new ALNS object starting from the current modelPath
+        // and Orienteering object
+        // SwingUtilities.invokeLater(currentALNS);
+        
+        // Now go back to "doInBackground()" and wait for the optimization to complete
+    }
+    
+    private void messageReceived(){
+        // Here I should publish the message I've received and add information
+        // on the batch size and instance number in the batch
+        this.setProgress(messageFromALNS.getProgress());
+        
+        OptimizationStatusMessage newMessage = new OptimizationStatusMessage(
+                lastModelPath,
+                messageFromALNS.getProgress(),
+                messageFromALNS.getElapsedTime(),
+                this.lastInstanceNumber,
+                this.modelPaths.size()
+        );
+        publish(newMessage);
+    }
+    
+    @Override
+    protected void process(List<OptimizationStatusMessage> messages){
+        if(messages != null && !messages.isEmpty())
+            System.out.println("Message from ALNS: "+messages.get(messages.size()-1)); // DEBUG
+        // here I should update the progress bar and the label in the gui
     }
 }

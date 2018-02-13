958f30a9a185d757d99fc7187c7399da3011af13
frashpikass <frashpikass@gmail.com>
2018-02-13 10:15:52 +0100

    Added an input verifier for an input which is >= 0

---
+++ src/solverView/bindingInterfaces/PositiveOrZeroIntegerValidator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solverView.bindingInterfaces;
+
+import org.jdesktop.beansbinding.Validator;
+
+/**
+ * Class which validates positive integers and ensures they are greater than 0.
+ * @author Frash
+ */
+public class PositiveOrZeroIntegerValidator extends Validator<Integer>{
+
+    @Override
+    public Validator.Result validate(Integer value) {
+        if (!(value >= 0 )) {
+            return new Validator.Result(value, "This value must be a positive integer or zero!");
+        }
+        
+        return null; 
+    }
+    
+}

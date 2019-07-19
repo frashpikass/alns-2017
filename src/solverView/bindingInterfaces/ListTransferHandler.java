ffb99ed6440fa1d93763cd07db3dd6260b4d6cb5
frashpikass <frashpikass@gmail.com>
2019-07-19 09:24:08 +0200

    Final Commit before delivery

---
+++ src/solverView/bindingInterfaces/ListTransferHandler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solverView.bindingInterfaces;
+
+/**
+ * Java Class to handle drag and drop in the instances List
+ * @author Frash
+ */
+public class ListTransferHandler {
+    
+}
